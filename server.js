91ffbe0f57e014d91e70139069c7df3a6fa645de
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-13 23:29:46 -0700

    Added route files

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express') //brings in express
+const connectDB = require('./config/db') //brings in db file
 
 const app = express() //initialize app variable with express
 
+//Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
 
 const PORT = process.env.PORT || 5000 //looks for environment variable called PORT to use for Heroku, if not, it will go to 5000
 
